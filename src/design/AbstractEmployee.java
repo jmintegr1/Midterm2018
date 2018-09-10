57a610571f4ed38c0e73aa0c11416361a2b87f36
jmintegr1 <jmintegr1@gmail.com>
2018-09-10 19:04:06 -0400

    Updated Design

--- src/design/AbstractEmployee.java
+++ src/design/AbstractEmployee.java
@@ -4,11 +4,11 @@ package design;
 
 public abstract class AbstractEmployee implements Employee {
 
-    public final String founderName = "Asif";
-    static String address = "31-10, 37th Avenue, Long Island City Astoria, NY";
+    public final String founderName = "Abdul Kudoz";
+    static String address = "250 Vesey Street, New York NY";
 
     public void describeCompany() {
-        System.out.println("In the year 2018 " + founderName + " founded the company located at " + address + ".");
+        System.out.println("The man with the vison Mr. " + founderName + " founded the company located at " + address + ".");
     }
     public abstract void describeCompany(String Mission);
 }
