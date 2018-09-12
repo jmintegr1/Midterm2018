d3cddeaba0251097d20b477a22cca07f3088cc27
jmintegr1 <jmintegr1@gmail.com>
2018-09-11 22:42:41 -0400

    Palindrome

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,20 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String plstr = "DAD";
+
+
+        String reverseStr = ""; // Objects of String class
+
+        int length = plstr.length();
+        for ( int i = length - 1; i >= 0; i-- )
+            reverseStr = reverseStr + plstr.charAt(i);
+
+        if (plstr.equals(reverseStr))
+            System.out.println(plstr + " is a palindrome.. Congrats!");
+        else
+            System.out.println(plstr + " is not a palindrome.. Try again!");
+
     }
 }
+
